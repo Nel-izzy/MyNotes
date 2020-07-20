f3bde396332aac62009aee82647a44418d19879e
nelson <blackcodedigitals@gmail.com>
2020-07-20 16:06:52 +0100

    Updates

---
+++ precache-manifest.db54d2e2ca3d30b16ee9a0de21beb6c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f076a0eeada6ac5ef601cddb8cefa516",
+    "url": "/MyNotes/index.html"
+  },
+  {
+    "revision": "c2ecfc651dfff19f7c54",
+    "url": "/MyNotes/static/css/main.f7101a79.chunk.css"
+  },
+  {
+    "revision": "d20afb9b0d865715a70a",
+    "url": "/MyNotes/static/js/2.175c4525.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyNotes/static/js/2.175c4525.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2ecfc651dfff19f7c54",
+    "url": "/MyNotes/static/js/main.1fb50781.chunk.js"
+  },
+  {
+    "revision": "52b339a8f34427ecb644",
+    "url": "/MyNotes/static/js/runtime-main.dd49911b.js"
+  }
+]);
